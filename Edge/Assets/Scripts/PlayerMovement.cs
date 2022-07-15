0a11bffe0d90aced489362ad71674a785fd97ff3
VgbHj <mishaslsk@gmail.com>
2022-07-16 01:06:55 +0300

    Add player direction move

--- Edge/Assets/Scripts/PlayerMovement.cs
+++ Edge/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed = 5;
+    public float rotationSpeed = 720;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector3.zero){
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed);
+        }
     }
 }
