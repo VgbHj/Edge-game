a7cc102bed18f57b22efe0d818d591bab1b97d82
VgbHj <mishaslsk@gmail.com>
2022-07-18 20:18:14 +0300

    Add jump

--- Edge/Assets/Scripts/PlayerMovement.cs
+++ Edge/Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,16 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 10;
     public float rotationSpeed = 1;
+    public float jumpButtonGracePeriod;
+    public float gravityMultiplier;
+    public float jumpHeight;
 
     private Animator animator;
     private CharacterController characterController;
+    private float ySpeed;
+    private float originalStepOffset;
+    private float? lastGroundedTime;
+    private float? jumpButtonPressedTime;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +35,38 @@ public class PlayerMovement : MonoBehaviour
         float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
         movementDirection.Normalize();
 
-        characterController.SimpleMove(movementDirection * magnitude);
+        float gravity = Physics.gravity.y * gravityMultiplier;
+        ySpeed += gravity * Time.deltaTime;
+
+        if (characterController.isGrounded){
+            lastGroundedTime = Time.time;
+        }
+
+        if (Input.GetButtonDown("Jump")){
+            jumpButtonPressedTime = Time.time;
+        }
+
+
+        if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
+        {
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+
+            if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)
+            {
+                ySpeed = Mathf.Sqrt(jumpHeight * -2 * gravity);;
+                jumpButtonPressedTime = null;
+                lastGroundedTime = null;
+            }
+        }
+        else{
+            characterController.stepOffset = 0;
+        }
+
+
+        Vector3 velocity = movementDirection * magnitude;
+        velocity.y = ySpeed;
+        characterController.Move(velocity * Time.deltaTime);
 
         if (movementDirection != Vector3.zero){
             animator.SetBool("IsMoving", true);
